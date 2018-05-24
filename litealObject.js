66894df53c02fe7618f6b551af8089b42846e213
IsmaelJdz <ismael_br7@hotmail.com>
2018-05-23 22:46:29 -0500

    Archivos base y ejemplo objeto literal JS

---
+++ litealObject.js
@@ -0,0 +1,25 @@
+;
+
+((c, d) => {
+    c('hola')
+
+    const perro = {
+        nombre: 'Bruno',
+        edad: '2',
+        raza: 'peluche',
+        genero: 'Macho',
+
+        ladrar: function () {
+            c('guauuu guauuu')
+        },
+        comer: function () {
+            c('comiendo crocketas')
+        },
+        dormir: function () {
+            c(`${this.nombre} esta durmiendo`)
+        }
+    }
+
+    perro.dormir()
+
+})(console.log, document)
